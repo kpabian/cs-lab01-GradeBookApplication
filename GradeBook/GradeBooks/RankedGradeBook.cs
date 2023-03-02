63d901100daa183887351baefb6fe0442e2db59c
Katarzyna <katarzyna.pabian357@gmail.com>
2023-03-02 17:31:51 +0100

    Overriding GetLetterGrade() in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,33 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int less = 0, more = 0;
+            foreach(Student student in Students)
+            {
+                if (averageGrade > student.AverageGrade)
+                    less++;
+                else
+                    more++;
+            }
+            if (less + more < 5)
+                throw new InvalidOperationException();
+            else
+            {
+                double rank = (double)more / (more + less);
+                if (rank <= 0.2)
+                    return 'A';
+                else if (rank <= 0.4)
+                    return 'B';
+                else if (rank <= 0.6)
+                    return 'C';
+                else if (rank <= 0.8)
+                    return 'D';
+                else
+                    return 'F';
+
+            }
+        }
     }
 }
